362d3dd53d7ac50d6d46a2a49b026ca723b085eb
Pema Malling <pemamalling@gmail.com>
2021-09-10 17:30:09 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace shaderception_standalone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("No program file passed!");
+                return;
+            }
+
+            // Make compiler
+            Compiler compiler = new Compiler();
+            compiler.input = new UnityEngine.UI.InputField();
+            compiler.output = new UnityEngine.UI.Text();
+            compiler.screenMat = new UnityEngine.Material();
+
+            // Compile program
+            compiler.input.text = File.ReadAllText(args[0]);
+            compiler.Compile();
+
+            // Dump output
+            Console.WriteLine(compiler.output.text);
+            File.WriteAllText(args[0] + ".out", compiler.output.text);
+        }
+    }
+}
---
+++ Stub.cs
@@ -0,0 +1,114 @@
+using System;
+
+namespace UnityEngine
+{
+    namespace UI
+    {
+        public class Text
+        {
+            public string text;
+        }
+
+        public class InputField : Text {}
+    }
+
+    public struct Vector4
+    {
+        public float x, y, z, w;
+
+        public Vector4(float x, float y, float z, float w)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        public static Vector4 operator *(Vector4 a, Vector4 b)
+        {
+            return new Vector4(a.x*b.x, a.y*b.y, a.z*b.z, a.w*b.w);
+        }
+
+        public static Vector4 operator *(Vector4 a, float b)
+        {
+            return new Vector4(a.x*b, a.y*b, a.z*b, a.w*b);
+        }
+
+        public float this[int a]
+        {
+            get
+            {
+                switch (a)
+                {
+                    case 0: return x;
+                    case 1: return y;
+                    case 2: return z;
+                    case 3: return w;
+                    default: throw new IndexOutOfRangeException();
+                }
+            }
+            set
+            {
+                switch (a)
+                {
+                    case 0: x = a; break;
+                    case 1: y = a; break;
+                    case 2: z = a; break;
+                    case 3: w = a; break;
+                    default: throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"({x}, {y}, {z}, {w})";
+        }
+
+        public static Vector4 zero = new Vector4(0, 0, 0, 0);
+    }
+
+    public class Material
+    {
+        public void SetVectorArray(string a, Vector4[] b) {}
+        public void SetVector(string a, Vector4 b) {}
+    }
+}
+
+namespace VRC
+{
+    namespace SDKBase {}
+    namespace SDK3
+    {
+        namespace Components {}
+    }
+    namespace Udon
+    {
+        namespace Common {}
+    }
+}
+
+namespace UdonSharp
+{
+}
+
+public class RecursiveMethodAttribute : Attribute {}
+public class UdonSyncedAttribute : Attribute {}
+public enum UdonInputEventArgs
+{
+    None
+}
+
+public class UdonSharpBehaviour
+{
+    public virtual void InputUse(bool a, UdonInputEventArgs b) {}
+    public virtual void InputJump(bool a, UdonInputEventArgs b) {}
+    public virtual void InputGrab(bool a, UdonInputEventArgs b) {}
+    public virtual void InputDrop(bool a, UdonInputEventArgs b) {}
+    public virtual void InputMoveHorizontal(float a, UdonInputEventArgs b) {}
+    public virtual void InputMoveVertical(float a, UdonInputEventArgs b) {}
+    public virtual void InputLookHorizontal(float a, UdonInputEventArgs b) {}
+    public virtual void InputLookVertical(float a, UdonInputEventArgs b) {}
+    public virtual void Interact() {}
+    public virtual void OnDeserialization() {}
+}
